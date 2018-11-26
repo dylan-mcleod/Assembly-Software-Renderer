f654c484cdeba96fdcda200e8ebd78a171ed0fcd
dylan <dylan0121@yahoo.com>
2018-11-26 09:55:45 +0000

    a rasterization function begins to take shape before your very eyes...

--- source.s
+++ source.s
@@ -90,6 +90,153 @@ xmm2packed8888:
 	add rsp, 0x18
 	ret
 
+#dst, xmm, xmm, xmm
+#uses xmm7
+#call shufps arg3, arg3, 0x00 to have it be just one value that's mixed  
+.macro mixxmm a, b, c, d
+	movups xmm7, fltOnes
+
+	subps  xmm7, \d
+	movaps \a,   \c
+	mulps  \a,   xmm7
+	
+	movaps xmm7, \b
+	mulps  xmm7, \d
+	addps  \a,   xmm7
+.endm
+
+#3 is the temp
+.macro swapxmm a, b, c
+	movaps \c, \a
+	movaps \a, \b
+	movaps \b, \c
+.endm
+
+.macro fabs a, temp
+	movups \temp, fltAbsMask
+	andps \a, \temp
+.endm	
+
+# rdi: bool topdown
+# xmm0: pos1 (should be [ (0,0,X,1), (width,height,X,1) ]
+# xmm1: pos2
+# xmm2: pos3
+# push pos3  [rbp+0x90]
+# push pos2  [rbp+0x80]
+# push pos1  [rbp+0x70]
+# push norm3 [rbp+0x60]
+# push norm2 [rbp+0x50]
+# push norm1 [rbp+0x40]
+# push col3  [rbp+0x30]
+# push col2  [rbp+0x20]
+# push col1  [rbp+0x10]
+raster:
+	push rbp
+	mov rbp, rsp
+	
+	#set dy, and prepare for a swap
+	cmp rdi, 1
+	je raster_topdown
+		#bottom up
+		mov r9, 1
+
+		movss xmm3, [rbp+0x84] # b.y
+		movss xmm4, [rbp+0x94] # c.y
+
+		movss xmm5, [rbp+0x74] # a.y
+		movss xmm6, [rbp+0x74] # a.y
+		
+	jmp raster_extopdown
+	raster_topdown:
+		mov r9, -1    # dy
+
+		movss xmm3, [rbp+0x74] # a.y
+		movss xmm4, [rbp+0x74] # a.y
+
+		movss xmm5, [rbp+0x84] # b.y
+		movss xmm6, [rbp+0x94] # c.y
+
+	raster_extopdown:
+	
+	comiss xmm3, xmm5
+	jge raster_noswap1
+	swapxmm  xmm0, xmm1, xmm3
+raster_noswap1:
+	comiss xmm4, xmm6
+	jge raster_noswap2
+	swapxmm  xmm0, xmm2, xmm3
+raster_noswap2:
+	
+	movaps [rbp+0x90], xmm2
+	movaps [rbp+0x80], xmm1
+	movaps [rbp+0x70], xmm0
+
+
+	movaps xmm3, xmm1
+	subps  xmm3, xmm0
+	movaps xmm4, xmm2
+	subps  xmm4, xmm0
+	unpcklps xmm3, xmm4
+	movhlps  xmm4, xmm3
+	divps    xmm3, xmm4
+	 
+	cvtsi2ss xmm5, r9d	# convert r9d to a float
+	shufps xmm5, xmm5, 0x00   # put it in all 4 positions
+	movaps [rbp-0x10], xmm5 # throw this on the stack for future use
+	
+	mulps xmm3, xmm5      # mb, mc
+	movaps [rbp-0x20], xmm3 # store these as locals
+	
+	
+	# todo: add ceils here if time permits
+	cmp rdi, 1
+	je raster_topdown2
+		#bottom up
+		#compute y_start
+		movss xmm7, fltZeros
+		minss xmm7, [rbp+0x74]
+		cvtss2si eax, xmm7
+		mov [rbp-0x30], eax # y_start
+
+		#compute y_end
+		movss xmm7, winHeight2
+		minss xmm7, [rbp+0x84]
+		minss xmm7, [rbp+0x94]
+		cvtss2si eax, xmm7
+		mov [rbp-0x34], eax # y_end
+	jmp raster_extopdown2
+	raster_topdown2:
+		#top down
+		#compute y_start
+		movss xmm7, winHeight2
+		minss xmm7, [rbp+0x74]
+		cvtss2si eax, xmm7
+		mov [rbp-0x30], eax  # y_start
+
+		#compute y_end
+		movss xmm7, fltZeros
+		maxss xmm7, [rbp+0x84]
+		maxss xmm7, [rbp+0x94]
+		cvtss2si eax, xmm7
+		mov [rbp-0x34], eax  # y_end
+	raster_extopdown2:
+	
+	# compute mid_dist
+	movss xmm6, [rbp-0x74] # xmm6 =  [0].y
+	subss xmm6, [rbp-0x34] # xmm6 -= y_end
+	fabs  xmm6, xmm7       # xmm6 = |xmm6|
+	
+	
+	mov rsp, rbp
+	pop rbp
+	ret
+
+
+
+
+
+
+
 
 
 #screw the stack up ~~a little~~ quite a bit, don't reference stack variables relative to rsp
@@ -197,10 +344,6 @@ main:
 
 	call inittexture
 
-	movups xmm0, fltTest
-	call xmm2packed8888
-	call printint
-
 
 
 
@@ -227,6 +370,67 @@ mainLoop:
 	mov rsi, 100
 	mov rdx, 0xFF0000FF
 	call drawpixel
+
+
+
+
+	lea rax, hardcodedtri
+	push [rax]
+	push [rax+0x8]
+	push [rax+0x10]
+	push [rax+0x18]
+	push [rax+0x20]
+	push [rax+0x28]
+	push [rax+0x30]
+	push [rax+0x38]
+	push [rax+0x40]
+	push [rax+0x48]
+	push [rax+0x50]
+	push [rax+0x58]
+	push [rax+0x60]
+	push [rax+0x68]
+	push [rax+0x70]
+	push [rax+0x78]
+	push [rax+0x80]
+	push [rax+0x88]
+	push [rax+0x90]
+	push [rax+0x98]
+	movups xmm0, [rax]
+	movups xmm1, [rax+0x10]
+	movups xmm2, [rax+0x20]
+	call raster
+	# rdi: bool topdown
+	# xmm0: pos1 (should be [ (0,0,X,1), (width,height,X,1) ]
+	# xmm1: pos2
+	# xmm2: pos3
+	# push pos3  [rbp+0x90]
+	# push pos2  [rbp+0x80]
+	# push pos1  [rbp+0x70]
+	# push col3  [rbp+0x60]
+	# push col2  [rbp+0x50]
+	# push col1  [rbp+0x40]
+	# push norm3 [rbp+0x30]
+	# push norm2 [rbp+0x20]
+	# push norm1 [rbp+0x10]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 eventLoop:
 	
@@ -299,8 +503,15 @@ quit:
 	syscall
 
 .data
+	hardcodedtri: .float 100,100,1,0, 200,200,2,0, 100,300,1,0,  0,0,-1,0, 0,0,-1,0, 0,0,-1,0,  1,0,0,1, 0,1,0,1, 0,0,1,1 
+
+
+	fltAbsMask: .int 0x7FFFFFFF, 0x7FFFFFFF, 0x7FFFFFFF, 0x7FFFFFFF
 	fltTest: .float 0.1, 1, 0.2, 0.8
 	normMul: .float 255, 255, 255, 255
+
+	fltOnes:  .float 1, 1, 1, 1
+	fltZeros: .float 0, 0, 0, 0
 
 	eventToProcess:	.space 56
 	globalWindowPtr: .space 8
@@ -312,9 +523,11 @@ quit:
 
 	running: .int 1
 
-	myobjects: .long 1,12, 0,0,0, 1,0,0
+	myobjects_spec: .long  1,1,0
+	myobjects_mem:  .float 0,0,0,0,0,1,1,1,1, 0,1,0,0,0,1,1,1,1, 1,0,0,0,0,1,1,1,1
 	winWidth: .quad 600
 	winHeight: .quad 400
+	winHeight2: .float 399
 	winTitle: .asciz ""
 	intformat: .asciz "here is an int: %i\n"
 	xmm0formatint: .asciz "xmm0 (int): %i, %i, %i, %i\n"
